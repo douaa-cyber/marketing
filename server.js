644a82e01b6b37be708f1b0f6988079a98888b55
douaa-cyber <129712465+douaa-cyber@users.noreply.github.com>
2024-08-12 11:35:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,14 +14,10 @@ const multer = require('multer');
 const jwt =require('jsonwebtoken');
 const app = express();
 
-<<<<<<< HEAD
-const port = 3000;
-const hostname = '192.168.2.66';
-=======
+
+
 const port = 10000;
 const hostname = '0.0.0.0';
->>>>>>> 7f2a87a7fff599d4841a86879819b2892e2f1932
-
 app.use(express.static('public'));
 
 //VIRIFER SI LE DIRECTORY EXIST
