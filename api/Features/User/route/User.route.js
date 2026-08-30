0049f39bcb1ccd956bf9dcca52ad32fb4f44439f
Douaa Agr <douinfo11@gmail.com>
2026-08-30 12:07:59 +0100

    Fix import case for authorize middleware

--- api/Features/User/route/User.route.js
+++ api/Features/User/route/User.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../../../middleware/Auth");
 const UserController = require("../controller/index");
-const authorize = require("../../../middleware/authorize");
+const authorize = require("../../../middleware/Authorize");
 
 router.get(
   "/profile",
