226d68d1cae7cf0b48e933546d0250abb107b4fa
Douaa Agr <douinfo11@gmail.com>
2026-08-30 14:07:19 +0100

    Fix case sensitivity in authorize middleware import

--- api/Features/Objectif/objectif.route.js
+++ api/Features/Objectif/objectif.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ObjectifController = require("./objectif.controller");
 const authMiddleware = require("../../middleware/Auth");
-const authorize = require("../../middleware/authorize");
+const authorize = require("../../middleware/Authorize");
 router.get(
   "/all",
   authMiddleware,
