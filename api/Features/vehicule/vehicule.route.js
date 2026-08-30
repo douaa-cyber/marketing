4f74772aa3fea4641249f6e3d0a6a5d9ac50038a
Douaa Agr <douinfo11@gmail.com>
2026-08-30 12:08:37 +0100

    Fix case sensitivity in authorize middleware import

--- api/Features/vehicule/vehicule.route.js
+++ api/Features/vehicule/vehicule.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../../middleware/Auth");
-const authorize = require("../../middleware/authorize");
+const authorize = require("../../middleware/Authorize");
 
 const VehiculeController = require("./controller/index");
 
